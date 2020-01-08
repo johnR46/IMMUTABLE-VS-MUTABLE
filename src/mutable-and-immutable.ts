54d77f6cc1af9020af0e2bc73ca36d65496cc938
John nonphala <johnnote7@gmail.com>
2020-01-08 22:58:41 +0700

    mutable-and-immutablemutable-and-immutable

--- src/mutable-and-immutable.ts
+++ src/mutable-and-immutable.ts
@@ -1,8 +1,31 @@
-let tomatoes = 3;
+// MUTABLE  CODE
 
-let apple = tomatoes;
+data => {
+  for (let i = 0; i < data.length; i++) {
+    data[i]['snippet'] = data[i]['targetName'];
+    delete data[i]['targetName'];
+    for (let j = 0; j < data[i].failingTapNeighbours.length; j++) {
+      data[i].failingTapNeighbours[j]['snippet'] =
+        data[i].failingTapNeighbours[j]['targetName'];
+      delete data[i].failingTapNeighbours[j]['targetName'];
+    }
+  }
+  return data;
+};
 
-tomatoes++;
-
-console.log(apple);
-console.log(tomatoes);
+formData => {
+  return formData.map(tapTarget => {
+    const { tagname, failingTapNeighbours, ...rest } = tapTarget;
+    return {
+      snippet: tagname,
+      ...rest,
+      failingTapNeighbours: failingTapNeighbours.map(neighbor => {
+        const { tagname, ...rest } = neighbor;
+        return {
+          snippet: tagname,
+          ...rest
+        };
+      })
+    };
+  });
+};
